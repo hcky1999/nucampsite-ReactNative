b5cb08f3a6046b2259a2f9614cc97e69489760ba
hcky1999 <heckladiv@icloud.com>
2021-03-20 01:42:44 -0700

    Persis Redux Store

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 console.disableYellowBox = true;
 
-
-const store = ConfigureStore();
+//bringing together process store and the store using object destructuring syntax
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
   return (
+      //wrap with persist gate component provided by redux-persist to help integrate react and react native apps it help application from rendering the until rehydrated from the client side storage
     <Provider store={store}>
-        <Main />  
+      <PersistGate
+                loading={<Loading />}
+                persistor={persistor}>
+                <Main />
+            </PersistGate>
     </Provider>
   ); 
 
